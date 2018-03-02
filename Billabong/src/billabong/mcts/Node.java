5b284335828bd66bb7d88b2124d6687e30990d5e
Kamil Inglot <outglot@gmail.com>
2018-03-02 16:30:54 +0100

    p

--- Billabong/src/billabong/mcts/Node.java
+++ Billabong/src/billabong/mcts/Node.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-import com.baeldung.algorithms.mcts.montecarlo.State;
+
 
 public class Node {
     State state;
