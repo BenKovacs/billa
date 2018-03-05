39513607366656bd0de139310406d033f65d7878
StefanToni <skatemusic2301@gmail.com>
2018-03-05 17:39:45 +0100

    mergedi merge

--- Billabong/src/billabong/mcts/MonteCarloTreeSearch.java
+++ Billabong/src/billabong/mcts/MonteCarloTreeSearch.java
@@ -133,14 +133,10 @@ public class MonteCarloTreeSearch {
             String x = "";
             for(int i = 0; i < tempNode.getState().getBoard().getKangaroos().size(); i++){
     			x = x + " Phase 4 Backpropagation  " + tempNode.getState().getBoard().getKangaroos().get(i).getX() + " x and y " + tempNode.getState().getBoard().getKangaroos().get(i).getY()  + "of the " + i + "th kanga";
-<<<<<<< HEAD
     			x = x + " current player is " + tempNode.getState().getPlayerNo();
-    			x = x + " amount of kangaroos " + tempNode.getState().getBoard().getKangaroos().size();
-    			System.lineSeparator();
-=======
-    			x = x + " current player is " + tempNode.getState().getPlayerNo() + System.lineSeparator(); 
+    			x = x + " amount of kangaroos " + tempNode.getState().getBoard().getKangaroos().size() + System.lineSeparator();
     			
->>>>>>> origin/master
+
     		}
             System.out.println(x);
             }
